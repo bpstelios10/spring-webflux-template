4a0eae2c19e146d0253827c95eae203748b44ecc
bpstelios10 <bpstelios10@hotmail.com>
2020-12-11 16:02:38 +0000

    [template] Got rid of MaxRAMFraction that was causing OOM in nfts

--- service/scripts/start_app.sh
+++ service/scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export JAVA_OPTIONS="-XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=32m -XX:ReservedCodeCacheSize=64m -XX:+UnlockExperimentalVMOptions -XX:MaxRAMFraction=1 -Xmx512M -Xms512M -Xss256k $EXTRA_JAVA_OPTIONS"
+export JAVA_OPTIONS="-XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=32m -XX:ReservedCodeCacheSize=64m -XX:+UnlockExperimentalVMOptions -Xmx512M -Xms512M -Xss256k $EXTRA_JAVA_OPTIONS"
 export GC_OPTIONS="-XX:+UseG1GC"
 
 echo "Starting Application"
