c88941eeeedacd23947d81580fecedc000a3c661
bpstelios10 <bpstelios10@hotmail.com>
2021-03-30 18:16:11 +0100

    [template] Fixed mocks configuration (async responses, that reduced threads needed)
    * Added nft mocks with higher delays
    * Added logs on mocks startup to log the arguments passed
    * Increased mocks pod memory and namespace quota

--- service-mocks/scripts/start_app.sh
+++ service-mocks/scripts/start_app.sh
@@ -18,14 +18,20 @@ JAVA_MONITORING_OPTIONS="-Dcom.sun.management.jmxremote
 export JAVA_OPTIONS="-XX:MaxMetaspaceSize=64m -XX:CompressedClassSpaceSize=16m -XX:ReservedCodeCacheSize=64m -XX:NativeMemoryTracking=summary -XX:+UnlockDiagnosticVMOptions -Xss256k -Xms512m -Xmx512m $JAVA_MONITORING_OPTIONS $EXTRA_JAVA_OPTIONS"
 export GC_OPTIONS="-XX:+UseParallelGC"
 
-if [[ -z "${WIREMOCK_OPTS}" ]]; then
-  WIREMOCK_OPTS="--https-port 8443 --port 9090 --container-threads=5000 --jetty-acceptor-threads=400 --async-response-enabled --async-response-threads=1000 --global-response-templating"
+if [[ -z $WIREMOCK_OPTS ]]; then
+  WIREMOCK_OPTS="--port 9090 --container-threads=500 --jetty-acceptor-threads=100 --global-response-templating"
   echo "WIREMOCK_OPTS has been overridden"
   WIREMOCK_OPTS="${WIREMOCK_OPTS}"
 fi
 
-WIREMOCK_OPTS="$WIREMOCK_OPTS --no-request-journal --root-dir /data/mappings"
+if [[ $DEPLOYMENT_ENVIRONMENT == *"-nft" || $DEPLOYMENT_ENVIRONMENT == "local" ]]; then
+    echo "Starting mocks from /data/mappings-nft root"
+    WIREMOCK_OPTS="$WIREMOCK_OPTS --no-request-journal --root-dir /data/mappings-nft"
+else
+    echo "Starting mocks from /data/mappings (normal FT mappings) root"
+    WIREMOCK_OPTS="$WIREMOCK_OPTS --max-request-journal-entries=1200 --root-dir /data/mappings"
+fi
 
-echo "Starting Wiremock"
+echo "Starting Wiremock with WIREMOCK_OPTS: [$WIREMOCK_OPTS]"
 
 exec java $JAVA_OPTIONS $GC_OPTIONS -cp service-mocks.jar com.github.tomakehurst.wiremock.standalone.WireMockServerRunner $WIREMOCK_OPTS
