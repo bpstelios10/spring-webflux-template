dc7d7a8d86b22f03639735e7e6113ac7cb53efeb
bpstelios10 <bpstelios10@hotmail.com>
2024-01-10 17:56:04 +0200

    [template] added observability aspect
    * used micrometer-tracing-bridge-brave and zipkin-reporter-brave

--- service/src/main/java/framework/templates/springbootwebflux/service/web/handler/HealthCheckHandler.java
+++ service/src/main/java/framework/templates/springbootwebflux/service/web/handler/HealthCheckHandler.java
@@ -1,5 +1,8 @@
 package framework.templates.springbootwebflux.service.web.handler;
 
+import io.micrometer.observation.annotation.Observed;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -8,10 +11,14 @@ import reactor.core.publisher.Mono;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@Slf4j
 @Component
+@Observed
 public class HealthCheckHandler implements HandlerFunction<ServerResponse> {
     @Override
     public Mono<ServerResponse> handle(ServerRequest request) {
+        log.info(MDC.get("traceId"));
+        log.info(MDC.get("spanId"));
         return Mono.just(OK.getReasonPhrase())
                 .flatMap(s -> ServerResponse.ok()
                         .bodyValue(s)
